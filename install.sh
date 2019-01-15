08ce84f096a3020775692c0b0d25288235a0d0eb
Taras Glek <taras@purestorage.com>
2019-01-15 08:54:17 -0800

    install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set +x +e
+sudo ln -sfv `pwd`/*-fzf /usr/local/bin/
+curl -L https://github.com/junegunn/fzf-bin/releases/download/0.17.5/fzf-0.17.5-linux_amd64.tgz | sudo tar -C /usr/local/bin -zxv
