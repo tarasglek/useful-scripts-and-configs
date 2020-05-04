6f6e548ad511679c67a8e56cc61db5d272337d45
Taras Glek <taras@purestorage.com>
2020-05-03 21:27:18 -0700

    simpler

--- run-or-focus.sh
+++ run-or-focus.sh
@@ -2,10 +2,4 @@
 set -x
 cmd=$1
 class=$2
-running_cmd=`[ ! -z "$3" ] && echo $3 || echo $cmd`
-pid=`pidof $running_cmd`
-if [ -z "$pid" ]; then
-    $cmd
-else
-    i3-msg "[class=$class] focus"
-fi
+i3-msg "[class=$class] focus" || $cmd
