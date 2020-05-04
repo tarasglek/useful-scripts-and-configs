89dc827e3f1ffe7f86d26417d89d4af6501c1d8c
Taras Glek <taras@purestorage.com>
2020-05-03 22:00:37 -0700

    stuff

--- run-or-focus.sh
+++ run-or-focus.sh
@@ -2,4 +2,12 @@
 set -x
 cmd=$1
 class=$2
-i3-msg "[class=$class] focus" || $cmd
+workspace=$3
+running_cmd=`[ ! -z "$4" ] && echo $4 || echo $cmd`
+pid=`pidof $running_cmd`
+if [ -z "$pid" ]; then
+    i3-msg "workspace $workspace"
+    $cmd
+else
+    i3-msg "[class=$class] focus"
+fi
