6752556c9a23255685402d96f9987d4b5d98ec5b
Taras Glek <taras@purestorage.com>
2020-05-21 11:06:24 -0700

    xdotool and marks

--- run-or-focus.sh
+++ run-or-focus.sh
@@ -3,14 +3,9 @@ set -x
 cmd=$1
 class=$2
 workspace=$3
-running_cmd=`[ ! -z "$4" ] && echo $4 || echo $cmd`
-pid=`pidof $running_cmd`
-if [ -z "$pid" ]; then
+if i3-msg "[con_mark=$class] focus"| jq '.[0]'.success|grep false ; then
     i3-msg "workspace $workspace"
     $cmd &
-    until xwininfo -tree -root ; do sleep 1; done
+    xdotool search --sync --onlyvisible --class $class
     i3-msg mark $class
-else
-    # i3-msg "[class=$class] focus"
-    i3-msg "[con_mark=$class] focus"
 fi
