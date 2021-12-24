e87007504ddf7cf4d51fded855ea0a6aba19e75d
Taras Glek <taras@glek.net>
2021-12-24 19:57:34 +0000

    more stuff

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@
 set +x +e
 sudo ln -sfv `pwd`/*-fzf `pwd`/*sh /usr/local/bin/
 curl -L https://github.com/junegunn/fzf-bin/releases/download/0.17.5/fzf-0.17.5-linux_amd64.tgz | sudo tar -C /usr/local/bin -zxv
+sudo apt install -y bat screen git ripgrep fd-find
+sudo ln -svf /usr/bin/fdfind /usr/local/bin/fd
