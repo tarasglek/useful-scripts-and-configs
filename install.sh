cf768c3fd3cfdd6c4781de0434486812898ca069
Taras Glek <taras@glek.net>
2025-01-02 13:46:17 +0200

    git-collapse

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set +x +e
-sudo ln -sfv `pwd`/*-fzf `pwd`/*sh /usr/local/bin/
+sudo ln -sfv $(pwd)/*-fzf $(pwd)/*sh $(pwd)/git-collapse /usr/local/bin/
 #curl -L https://github.com/junegunn/fzf-bin/releases/download/0.17.5/fzf-0.17.5-linux_amd64.tgz | sudo tar -C /usr/local/bin -zxv
 #sudo apt install -y bat screen git ripgrep fd-find psmisc htop
 #sudo ln -svf /usr/bin/fdfind /usr/local/bin/fd
